f6fa12f5a3dd4bbbf194438e39ec0a7bb3850322
Juanse <juanse@juanse-VirtualBox>
2017-04-24 20:26:30 +0200

    Routes fixed, json sended with selected schedule

--- config/routes.rb
+++ config/routes.rb
@@ -3,28 +3,24 @@ Rails.application.routes.draw do
   root 'static_pages#home'
 
   get 'static_pages/home'
-
   get 'static_pages/tour'
-
   get 'static_pages/contact'
-
   get 'static_pages/pricing'
-
   get 'static_pages/testimonials'
 
-  get ':name', to: 'entities#listf'
-
-  # Aquí es donde objento el JSON de cada horario
-  #get '/horarios', to: 'fields#get_schedule'
 
   devise_for :users
 
-  resources :entities do
-    resources :contacts
-    resources :advertisers
-    resources :fields do
-      resources :schedule
-    end
+  resources :entities
+  resources :contacts
+  resources :advertisers
+  resources :fields do
+    resources :schedule
   end
+  
+get '/horarios', to: 'fields#get_schedule'
+get ':name', to: 'entities#listf'
+  # Aquí es donde objento el JSON de cada horario
+
 
 end
